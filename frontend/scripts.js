13f6c1e80e10a690adc40b5cec3e4d9239dc5b9b
kandeel679 <youssifk000@gmail.com>
2026-03-06 16:13:47 +0200

    Add multiple identical `for` loops.

--- frontend/scripts.js
+++ frontend/scripts.js
@@ -3,3 +3,267 @@ console.log("hello a7eh");
 for (let i = 0; i < 10; i++) {
     console.log(i);
 }
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
